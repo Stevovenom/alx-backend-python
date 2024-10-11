0ce53fe738fb27731e359d1fa03dc41fa5b31e69
stevovenom <stephenokoth2002@gmail.com>
2024-10-11 19:14:59 +0300

    fix

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,7 +6,7 @@ type-annoted function sum_list which takes input_list of float as argument
 from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Args:
         input_list (float): lists of floats as arguments
