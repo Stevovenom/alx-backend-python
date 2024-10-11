4738b46d14e70c6df116da180a2ec215534ce715
stevovenom <stephenokoth2002@gmail.com>
2024-10-11 19:04:48 +0300

    takes input_list of floats as argument

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+type-annoted function sum_list which takes input_list of float as argument
+"""
+
+
+def sum_list(input_list: float) -> float:
+    return (sum(input_list))
