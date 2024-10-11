d8415a3551882908da5a3cba4c9f193d3629ba4f
stevovenom <stephenokoth2002@gmail.com>
2024-10-11 18:13:37 +0300

    concat type-annoted string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+concat function that takes string str1 and str2 as arguments
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Args:
+        str1 (string): the first string
+        str2 (string): the second string
+
+    Returns:
+        str: A concatenated string
+    """
+    return (str1 + str2)
