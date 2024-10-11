d5c23d3d28d6c19c6f9b77e8f7b671a6ca1b2505
stevovenom <stephenokoth2002@gmail.com>
2024-10-12 01:44:51 +0300

    To augment the safe_first_element function with proper duck-typed annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+augmenting teh given code  with the correct duck-typed annotation
+"""
+
+
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns the first element of a sequence or None if the sequence is empty."""
+    if lst:
+        return lst[0]
+    else:
+        return None
