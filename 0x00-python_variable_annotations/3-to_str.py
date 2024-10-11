402cb35eeee096a3cf56185fdbbd5efe28f5acc8
stevovenom <stephenokoth2002@gmail.com>
2024-10-11 18:45:29 +0300

    takes float n as argument and returns str representation of the float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+type-annoted function to_str that takes a float n
+"""
+
+
+def to_str (n: float) -> str:
+    """
+    Args:
+        n (float): the argument being passed
+    Returns:
+        str: string representation of the float
+    """
+    return (str(n))
