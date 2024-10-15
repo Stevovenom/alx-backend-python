95e9277ac10220a07ade119b716690213f673c93
stevovenom <stephenokoth2002@gmail.com>
2024-10-16 00:34:06 +0300

    fix update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,12 +3,12 @@
 A coroutine called async_generator that takes no arguments
 """
 
-from typing import Generator
+from typing import AsyncGenerator
 import asyncio
 import random
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
