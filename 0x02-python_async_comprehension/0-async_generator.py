2c629c8e7c309c265b92a410d9ef116ee0353b03
stevovenom <stephenokoth2002@gmail.com>
2024-10-16 00:39:59 +0300

    fix update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,12 +3,20 @@
 A coroutine called async_generator that takes no arguments
 """
 
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Asynchronous generator function that yields a random float between 0 and
+    10 after a 1 second delay for a total of 10 iterations
+
+    Returns:
+        Generator: Asynchronous generator object that can be used in an
+        awaitable context.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
